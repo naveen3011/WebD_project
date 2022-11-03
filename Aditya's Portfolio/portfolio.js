c171016374212735dd4d1e1a04a12e724420ae06
Naveen kushwaha <naveenkushwaha000@gmail.com>
2022-11-03 23:38:29 +0530

    Delete Aditya's Portfolio directory

--- Aditya's Portfolio/portfolio.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
